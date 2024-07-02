145beabda744f27f45b4f30640a7586406cfbd19
niferu <wargacki.patryq@gmail.com>
2024-07-02 12:25:40 +0200

    initial

---
+++ test/Auctioner.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Auctioner} from "../src/Auctioner.sol";
+
+contract AuctionerTest is Test {
+    Auctioner public auctioner;
+
+    function setUp() public {
+        auctioner = new Auctioner();
+    }
+}
