93d109b60cd047346aab0573026d4d47178a2df8
niferu <wargacki.patryq@gmail.com>
2024-07-02 12:25:34 +0200

    initial

---
+++ script/Auctioner.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.25;
+
+import {Script, console} from "forge-std/Script.sol";
+
+contract AuctionerScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.broadcast();
+    }
+}
