4a7d6ef02af7bbe4bd56d85881628b4281831015
niferu <wargacki.patryq@gmail.com>
2024-07-04 11:39:29 +0200

    refactor

--- src/interfaces/IAuctioner.sol
+++ src/interfaces/IAuctioner.sol
@@ -2,6 +2,52 @@
 pragma solidity ^0.8.25;
 
 interface IAuctioner {
+    /// @dev Errors
+    error Auctioner__AuctionDoesNotExist();
+    error Auctioner__AuctionNotOpened();
+    error Auctioner__InsufficientPieces();
+    error Auctioner__NotEnoughFunds();
+    error Auctioner__TransferFailed();
+
+    /// @dev Enums
+    enum AuctionState {
+        UNINITIALIZED,
+        PLANNED,
+        OPENED,
+        CLOSED,
+        FAILED,
+        VOTING,
+        FINISHED,
+        ARCHIVED
+    }
+
+    /// @dev Structs
+    struct Auction {
+        address asset;
+        string uri; // we need to pass it into FractAsset.sol
+        uint256 price;
+        uint256 pieces;
+        uint256 available;
+        uint256 max;
+        uint256 openTs;
+        uint256 closeTs;
+        address[] assetOwners; // we can get it from NFT
+        mapping(address => uint) ownerToFunds; // we can get it from NFT
+        address recipient;
+        AuctionState auctionState;
+    }
+
+    /// @dev Events
+    event Create();
+    event Plan();
+    event Purchase();
+    event Buyout();
+    event Claim();
+    event Refund();
+    event Vote(); // Check if event is available in gov
+    event TransferToBroker(address indexed wallet, uint256 indexed amount);
+    event StateChange(uint256 indexed auction, AuctionState indexed state);
+
     /// @notice Allows buying pieces of asset auctioned by broker
     /// @param id Auction id that we want to interact with
     /// @dev Emits Purchase event and TransferToBroker event if last piece has been bought
