6cfac305511d9b5c3a89755fc86768e7643160f0
niferu <wargacki.patryq@gmail.com>
2024-07-05 13:35:16 +0200

    updated with ownable

--- src/FractAsset.sol
+++ src/FractAsset.sol
@@ -7,13 +7,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/governance/utils/Votes.sol";
 import "@openzeppelin/contracts/governance/utils/IVotes.sol";
 
-contract FractAsset is ERC721A, ERC721ABurnable, EIP712, Votes {
+contract FractAsset is ERC721A, ERC721ABurnable, EIP712, Votes, Ownable {
     /// @dev ERROR!
     /// @dev Consider case when, tokenTransfer is peformed during voting (original owner already voted then transferred token)
     /// @dev check if buyer of token has vote also
 
     // We are getting 'name' and 'symbol' from Auctioner.sol
-    constructor(address initialOwner) ERC721A("MyToken", "MTK") EIP712("MyToken", "MTK") {}
+    constructor(address initialOwner) ERC721A("MyToken", "MTK") EIP712("MyToken", "MTK") Ownable(msg.sender) {}
 
     function _baseURI() internal pure override returns (string memory) {
         return "https";
@@ -59,40 +59,3 @@ contract FractAsset is ERC721A, ERC721ABurnable, EIP712, Votes {
         return interfaceId == type(IVotes).interfaceId || super.supportsInterface(interfaceId);
     }
 }
-
-/// @dev ERC721 VERSION
-
-// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Votes.sol";
-
-// contract FractAsset is ERC721, ERC721Enumerable, ERC721Burnable, Ownable, EIP712, ERC721Votes {
-//     // We are getting 'name' and 'symbol' from Auctioner.sol
-//     constructor(address initialOwner) ERC721("MyToken", "MTK") Ownable(initialOwner) EIP712("MyToken", "1") {}
-
-//     // We are getting uri from Auctioner.sol
-//     function _baseURI() internal pure override returns (string memory) {
-//         return "https";
-//     }
-
-//     function safeMint(address to, uint256 tokenId) external onlyOwner {
-//         _safeMint(to, tokenId);
-//     }
-
-//     // The following functions are overrides required by Solidity.
-
-//     function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Enumerable, ERC721Votes) returns (address) {
-//         return super._update(to, tokenId, auth);
-//     }
-
-//     function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable, ERC721Votes) {
-//         super._increaseBalance(account, value);
-//     }
-
-//     function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
-//         return super.supportsInterface(interfaceId);
-//     }
-// }
