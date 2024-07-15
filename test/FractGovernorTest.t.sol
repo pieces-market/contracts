b1bd8d37d2d8be7b70fd88acbdec3c704c3e4c55
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-07-15 14:27:22 +0200

    initial development tests

---
+++ test/FractGovernorTest.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FractGovernor} from "../src/FractGovernor.sol";
+import {FractAsset} from "../src/FractAsset.sol";
+
+contract FractGovernorTest is Test {}
