4dae8dde79c7ab4be5fc360656d3be54ec6625d0
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-07-16 15:22:41 +0200

    planning

--- src/CustomGovernor.sol
+++ src/CustomGovernor.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.25;
 
-contract CustomGovernor {}
+/// @dev This contract only will be allowed to execute buyout function from Auctioner
+contract CustomGovernor {
+    /// @dev FUNCTION execute
+    /// @dev FUNCTION delegate
+    /// @dev FUNCTION vote
+    /// @dev FUNCTION getVotes
+    /// @dev FUNCTION votingPeriod
+    /// @dev FUNCTION quorum = 51%
+
+    constructor() {}
+}
