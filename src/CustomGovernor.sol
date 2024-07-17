424ee3f4d5311002ca86f0a94e1c65183c999a30
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-07-17 19:18:55 +0200

    building core

--- src/CustomGovernor.sol
+++ src/CustomGovernor.sol
@@ -8,6 +8,9 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract CustomGovernor is Ownable {
     /// @dev FUNCTION quorum = 51%
 
+    error Governor__ProposalNotActive();
+    error Governor__ProposalDoesNotExist();
+
     enum ProposalState {
         Inactive,
         Active,
@@ -15,6 +18,7 @@ contract CustomGovernor is Ownable {
         Failed
     }
 
+    /// @dev ???
     enum VoteType {
         For,
         Against,
@@ -22,58 +26,78 @@ contract CustomGovernor is Ownable {
     }
 
     struct ProposalCore {
+        address assset;
         uint256 timeLeft;
-        bool executed;
-        bool canceled;
-    }
-
-    struct ProposalVote {
-        uint256 againstVotes;
+        ProposalState state;
         uint256 forVotes;
+        uint256 againstVotes;
         uint256 abstainVotes;
         mapping(address => bool) hasVoted;
     }
 
-    mapping(uint256 => ProposalCore) private _proposals;
-    mapping(uint256 proposalId => ProposalVote) private _proposalVotes;
+    /// @dev Variables
+    uint256 private _totalProposals;
 
+    /// @dev Mappings
+    mapping(uint256 proposalId => ProposalCore) private _proposals;
+
+    /// @dev Constructor
     constructor(address owner) Ownable(owner) {}
 
-    function propose() external {}
+    function propose() external onlyOwner {
+        ProposalCore storage proposal = _proposals[_totalProposals];
 
-    function execute() external {}
+        proposal.state = ProposalState.Active;
 
-    function cancel() external {}
+        _totalProposals += 1;
+    }
+
+    function execute() external onlyOwner {}
+
+    function cancel(uint proposalId) external onlyOwner {
+        if (proposalId >= _totalProposals) revert Governor__ProposalDoesNotExist();
+        ProposalCore storage proposal = _proposals[proposalId];
+
+        proposal.state = ProposalState.Failed;
+    }
 
     function delegate() external {}
 
-    function vote() external {}
+    function castVote(uint proposalId, VoteType vote) external {
+        if (proposalId >= _totalProposals) revert Governor__ProposalDoesNotExist();
+        ProposalCore storage proposal = _proposals[proposalId];
+        if (proposal.state != ProposalState.Active) revert Governor__ProposalNotActive();
 
-    function getVotes() external {}
-
-    function votingPeriod() external {}
-
-    /// @dev SimpleVoting
-
-    function proposalVotes(uint256 proposalId) public view returns (uint256 againstVotes, uint256 forVotes, uint256 abstainVotes) {
-        ProposalVote storage proposalVote = _proposalVotes[proposalId];
-
-        return (proposalVote.againstVotes, proposalVote.forVotes, proposalVote.abstainVotes);
+        if (vote == VoteType.For) proposal.forVotes += 1;
     }
 
     function quorumReached(uint256 proposalId) internal view returns (bool) {
-        ProposalVote storage proposalVote = _proposalVotes[proposalId];
+        ProposalCore storage proposal = _proposals[proposalId];
 
-        return totalVotes(proposalId) <= proposalVote.forVotes + proposalVote.abstainVotes;
-    }
-
-    function totalVotes(uint256 /* proposalId */) internal pure returns (uint256) {
-        return 0;
+        return totalVotes(proposalId) <= proposal.forVotes + proposal.abstainVotes;
     }
 
     function voteSucceeded(uint256 proposalId) internal view returns (bool) {
-        ProposalVote storage proposalVote = _proposalVotes[proposalId];
+        ProposalCore storage proposal = _proposals[proposalId];
 
-        return proposalVote.forVotes > proposalVote.againstVotes;
+        return proposal.forVotes > proposal.againstVotes;
+    }
+
+    /// @dev Getter
+    function getVotes() external {}
+
+    /// @dev Getter
+    function votingPeriod() external {}
+
+    /// @dev Getter
+    function proposals(uint256 proposalId) public view returns (uint256 againstVotes, uint256 forVotes, uint256 abstainVotes) {
+        ProposalCore storage proposal = _proposals[proposalId];
+
+        return (proposal.againstVotes, proposal.forVotes, proposal.abstainVotes);
+    }
+
+    /// @dev Getter
+    function totalVotes(uint256 /* proposalId */) internal pure returns (uint256) {
+        return 0;
     }
 }
