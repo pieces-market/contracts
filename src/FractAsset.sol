d5e02f468b75b6a91d0a1eaf03282247fc581d25
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-07-19 16:08:10 +0200

    fixing imports

--- src/FractAsset.sol
+++ src/FractAsset.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.25;
 
 import "@ERC721A/contracts/ERC721A.sol";
-/// @dev Do we need this? ERC721A has built in burn fn
-// import "@ERC721A/contracts/extensions/ERC721ABurnable.sol";
+import {ERC721AVotes} from "./ERC721AVotes.sol";
+import "@ERC721A/contracts/extensions/ERC721ABurnable.sol";
 import "@ERC721A/contracts/extensions/ERC721AQueryable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/governance/utils/Votes.sol";
@@ -12,7 +12,7 @@ import "@openzeppelin/contracts/governance/utils/IVotes.sol";
 /// @dev ERC404
 
 /// @dev ERC721A version of Asset (NFT) contract with cheap multiple minting function
-contract FractAsset is ERC721A, ERC721AQueryable, EIP712, Votes, Ownable {
+contract FractAsset is ERC721A, ERC721AQueryable, ERC721ABurnable, EIP712, ERC721AVotes, Ownable {
     /// @dev ERROR!
     /// @dev Consider case when, tokenTransfer is peformed during voting (original owner already voted then transferred token)
     /// @dev check if buyer of token has vote also
@@ -27,7 +27,6 @@ contract FractAsset is ERC721A, ERC721AQueryable, EIP712, Votes, Ownable {
 
     /// @dev Override tokenURI to keep 1 URI for all tokens?
     // This will lead to Metadata, which will be unique for each token
-    // There will be 'image' field in Metadata that will be same for all tokens per asset
     function _baseURI() internal view override returns (string memory) {
         return baseURI;
     }
@@ -47,32 +46,14 @@ contract FractAsset is ERC721A, ERC721AQueryable, EIP712, Votes, Ownable {
         }
     }
 
-    /// @dev WE NEED TO OVERRIDE BURN, DELEGATE etc.!!! (onlyOwner)
-    ///
-    ///
+    /// @dev The following functions are overrides required by Solidity
 
-    /// @dev Delegate needs to be called to assign votes
-    // function delegateVotes(address delegatee) external {
-    //     _delegate(delegatee, delegatee);
-    // }
-
-    /// @dev See {ERC721-_afterTokenTransfer}. Adjusts votes when tokens are transferred.
-    /// @dev Instead of {_afterTokenTransfer} that is used in ERC721, ERC721A uses {_afterTokenTransfers} (with an 's')
-    /// @dev Emits a {IVotes-DelegateVotesChanged} event.
-    /// @dev This function is corrupting burn
-    // function _afterTokenTransfers(address from, address to, uint256 firstTokenId, uint256 batchSize) internal virtual override {
-    //     _transferVotingUnits(from, to, batchSize);
-    //     _delegate(to, to);
-    //     super._afterTokenTransfers(from, to, firstTokenId, batchSize);
-    // }
-
-    /// @dev Returns the balance of `account`.
-    /// @dev WARNING: Overriding this function will likely result in incorrect vote tracking.
-    function _getVotingUnits(address account) internal view virtual override returns (uint256) {
-        return balanceOf(account);
+    function _afterTokenTransfers(address from, address to, uint256 startTokenId, uint256 quantity) internal virtual override(ERC721A, ERC721AVotes) {
+        super._afterTokenTransfers(from, to, startTokenId, quantity);
     }
 
-    /// @dev Check if we indeed need it
+    /// @dev Check if we indeed need this -> if ERC721AQueryable included override(ERC721A, IERC721A)
+    ///
     /// @dev ERC721a Governance Token Interface Support
     /// @dev Implements the interface support check for ERC721a Governance Token
     /// @notice Checks if the contract implements an interface you query for, including ERC721A and Votes interfaces
