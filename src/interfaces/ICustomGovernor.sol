445273127c6cbf272e17121f3ea198749fcfd87f
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-07-19 18:49:30 +0200

    interface update

--- src/interfaces/ICustomGovernor.sol
+++ src/interfaces/ICustomGovernor.sol
@@ -1,4 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.25;
 
-interface ICustomGovernor {}
+interface ICustomGovernor {
+    error Governor__ProposalNotActive();
+    error Governor__ProposalDoesNotExist();
+    error Governor__TokenAlreadyUsedForVoting(uint proposalId, uint tokenId);
+
+    enum ProposalState {
+        Inactive,
+        Active,
+        Passed,
+        Failed
+    }
+
+    enum VoteType {
+        For,
+        Against,
+        Abstain
+    }
+
+    event Propose(uint indexed id, address indexed asset, uint indexed deadline, string description);
+}
