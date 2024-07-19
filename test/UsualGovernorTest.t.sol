2c5e65fbf68de29732b55fa2c9ad4b47186d145e
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-07-19 17:42:34 +0200

    rename

--- test/UsualGovernorTest.t.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.25;
-
-import {Test, console} from "forge-std/Test.sol";
-import {UsualGovernor} from "../src/UsualGovernor.sol";
-import {FractAsset} from "../src/FractAsset.sol";
-
-contract UsualGovernorTest is Test {}
