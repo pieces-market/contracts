fdb9f996c7b61830fe9141c529ed62a339a2ac64
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-07-22 17:58:05 +0200

    broker added

--- test/AuctionerTest.t.sol
+++ test/AuctionerTest.t.sol
@@ -2,63 +2,57 @@
 pragma solidity ^0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
 import {Auctioner} from "../src/Auctioner.sol";
 import {Asset} from "../src/Asset.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IAuctioner} from "../src/interfaces/IAuctioner.sol";
 
 contract AuctionerTest is Test {
-    Auctioner public auctioner;
+    Auctioner private auctioner;
+    Asset private asset;
 
     uint256 private constant STARTING_BALANCE = 100 ether;
 
     address private OWNER = makeAddr("owner");
+    address private BROKER = makeAddr("broker");
     address private USER = makeAddr("user");
     address private BUYER = makeAddr("buyer");
     address private DEVIL = makeAddr("devil");
 
     function setUp() public {
-        vm.prank(OWNER);
+        vm.startPrank(OWNER);
         auctioner = new Auctioner();
+
+        address precomputedAsset = vm.computeCreateAddress(address(auctioner), vm.getNonce(address(auctioner)));
+
+        vm.recordLogs();
+        vm.expectEmit(true, true, true, true, address(auctioner));
+        emit IAuctioner.Create(0, precomputedAsset, 2 ether, 100, 10, block.timestamp, block.timestamp + 7 days, BROKER);
+        auctioner.create("Asset", "AST", "https:", 2 ether, 100, 10, block.timestamp, block.timestamp + 7 days, BROKER);
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        address createdAsset = address(uint160(uint256(entries[2].topics[2])));
+        asset = Asset(createdAsset);
+        vm.stopPrank();
+
         console.log("Auctioner: ", address(auctioner));
+        console.log("Asset: ", address(asset));
 
         deal(OWNER, STARTING_BALANCE);
+        deal(BROKER, STARTING_BALANCE);
         deal(USER, STARTING_BALANCE);
         deal(BUYER, STARTING_BALANCE);
         deal(DEVIL, STARTING_BALANCE);
     }
 
-    function test_setup() public {
-        string memory name = "auction";
-        string memory symbol = "auc";
-        string memory uri = "https";
-        uint price = 2 ether;
-        uint pieces = 100;
-        uint max = 5;
-        uint start = block.timestamp;
-        uint end = block.timestamp + 7 days;
-        address rec = DEVIL;
-
-        vm.prank(OWNER);
-        auctioner.create(name, symbol, uri, price, pieces, max, start, end, rec);
-
-        uint tokens;
-        tokens = auctioner.getTokens(0, BUYER);
-        assertEq(tokens, 0);
-
-        vm.prank(BUYER);
+    function testCanBuyPiecesAndReceiveVotePower() public {
+        vm.prank(USER);
         auctioner.buy{value: 6 ether}(0, 3);
 
-        /// @dev Changing auction state to failed, so refunds are available
-        auctioner.stateHack(0, 4);
+        assertEq(3, Asset(asset).getVotes(USER));
+    }
 
-        vm.prank(BUYER);
-        auctioner.refund(0);
-
-        tokens = auctioner.getTokens(0, BUYER);
-        assertEq(tokens, 0);
-
-        vm.expectRevert(IAuctioner.Auctioner__InsufficientFunds.selector);
-        vm.prank(BUYER);
-        auctioner.refund(0);
+    modifier mod() {
+        _;
     }
 }
