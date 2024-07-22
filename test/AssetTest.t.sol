cf3fc6415db7c44c646abc502cfb07d6eea981a8
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-07-22 17:57:34 +0200

    Asset tests

---
+++ test/AssetTest.t.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {Auctioner} from "../src/Auctioner.sol";
+import {Asset} from "../src/Asset.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IAuctioner} from "../src/interfaces/IAuctioner.sol";
+
+contract AssetTest is Test {
+    Auctioner private auctioner;
+    Asset private asset;
+
+    uint256 private constant STARTING_BALANCE = 100 ether;
+
+    address private OWNER = makeAddr("owner");
+    address private BROKER = makeAddr("broker");
+    address private USER = makeAddr("user");
+    address private BUYER = makeAddr("buyer");
+    address private DEVIL = makeAddr("devil");
+
+    function setUp() public {
+        vm.startPrank(OWNER);
+        auctioner = new Auctioner();
+
+        address precomputedAsset = vm.computeCreateAddress(address(auctioner), vm.getNonce(address(auctioner)));
+
+        vm.recordLogs();
+        vm.expectEmit(true, true, true, true, address(auctioner));
+        emit IAuctioner.Create(0, precomputedAsset, 2 ether, 100, 10, block.timestamp, block.timestamp + 7 days, BROKER);
+        auctioner.create("Asset", "AST", "https:", 2 ether, 100, 10, block.timestamp, block.timestamp + 7 days, BROKER);
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        address createdAsset = address(uint160(uint256(entries[2].topics[2])));
+        asset = Asset(createdAsset);
+        vm.stopPrank();
+
+        console.log("Auctioner: ", address(auctioner));
+        console.log("Asset: ", address(asset));
+
+        deal(OWNER, STARTING_BALANCE);
+        deal(BROKER, STARTING_BALANCE);
+        deal(USER, STARTING_BALANCE);
+        deal(BUYER, STARTING_BALANCE);
+        deal(DEVIL, STARTING_BALANCE);
+    }
+
+    function testCanDoo() public {}
+
+    modifier mod() {
+        _;
+    }
+}
