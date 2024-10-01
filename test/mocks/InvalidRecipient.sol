8a5af8c65f37ebd25b09bd5c8e0cee3f31439640
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-10-01 16:34:23 +0200

    recipient without fallback fn to mock failed crypto transfers

---
+++ test/mocks/InvalidRecipient.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.25;
+
+/// @title Dummy contract that always reverts
+/// @notice Used as a placeholder to ensure reverts on attempted calls
+contract InvalidRecipient {
+
+}
