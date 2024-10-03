edf224e08e72fdad5e7741df8a492cf91768b7c9
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-10-03 15:11:36 +0200

    recipient adjusted to handle error case for propose fn

--- test/mocks/InvalidRecipient.sol
+++ test/mocks/InvalidRecipient.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.25;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+import {IAuctioner} from "../../src/interfaces/IAuctioner.sol";
+
 /// @title Dummy contract that always reverts
 /// @notice Used as a placeholder to ensure reverts on attempted calls
-contract InvalidRecipient {
+contract InvalidRecipient is Ownable {
+    constructor() Ownable(msg.sender) {}
 
+    function propose(uint, address, string memory, bytes memory) external pure {
+        revert IAuctioner.Auctioner__FunctionCallFailed();
+    }
 }
