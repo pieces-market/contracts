86636a86940d5fb368657465ce3e464c877cf626
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-10-04 16:38:39 +0200

    ERC721Receiver added to InvalidRecipient to manage more tests cases

--- test/mocks/InvalidRecipient.sol
+++ test/mocks/InvalidRecipient.sol
@@ -4,13 +4,23 @@ pragma solidity ^0.8.25;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 import {IAuctioner} from "../../src/interfaces/IAuctioner.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 /// @title Dummy contract that always reverts
 /// @notice Used as a placeholder to ensure reverts on attempted calls
-contract InvalidRecipient is Ownable {
+contract InvalidRecipient is Ownable, IERC721Receiver {
     constructor() Ownable(msg.sender) {}
 
     function propose(uint, address, string memory, bytes memory) external pure {
         revert IAuctioner.Auctioner__FunctionCallFailed();
+    }
+
+    function onERC721Received(
+        address /* operator */,
+        address /* from */,
+        uint256 /* tokenId */,
+        bytes memory /* data */
+    ) public pure override returns (bytes4) {
+        return this.onERC721Received.selector;
     }
 }
