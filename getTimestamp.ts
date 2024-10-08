5be5771f2bf0fdc6e9073471645c1fb263d71426
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-10-08 19:56:45 +0200

    script moved to api

--- getTimestamp.ts
+++
@@ -1,7 +0,0 @@
-function getUnixTimestamp(year: number, month: number, day: number, hour: number, minute: number, second: number) {
-    const date = new Date(year, month - 1, day, hour, minute, second)
-
-    return Math.floor(date.getTime() / 1000)
-}
-
-console.log(`Timestamp: ${getUnixTimestamp(2024, 8, 2, 0, 0, 0)}`)
