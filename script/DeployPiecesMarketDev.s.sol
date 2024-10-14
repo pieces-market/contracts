e8967d28eb6e168c9fe61e54f94db47572363d6c
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-10-14 17:56:26 +0200

    developers contracts version deploy script

---
+++ script/DeployPiecesMarketDev.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.25;
+
+import {Script, console} from "forge-std/Script.sol";
+import {AuctionerDev} from "../src/helpers/AuctionerDev.sol";
+import {GovernorDev} from "../src/helpers/GovernorDev.sol";
+
+contract DeployPiecesMarketDev is Script {
+    /// @dev EXCLUDE FROM COVERAGE
+    function test() public {}
+
+    address private foundation = vm.addr(vm.envUint("FOUNDATION_KEY"));
+
+    function run() external returns (AuctionerDev, GovernorDev) {
+        uint deployerKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerKey);
+        GovernorDev governor = new GovernorDev();
+        console.log("Governor Deployed:", address(governor));
+
+        AuctionerDev auctioner = new AuctionerDev(foundation, address(governor));
+        console.log("Auctioner Deployed:", address(auctioner));
+
+        governor.transferOwnership(address(auctioner));
+        console.log("Governor New Owner:", address(auctioner));
+        vm.stopBroadcast();
+
+        return (auctioner, governor);
+    }
+}
