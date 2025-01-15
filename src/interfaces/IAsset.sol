d04e45bfbb3a209a219f038ca4464d6992cf8010
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-01-15 18:06:09 +0100

    interface for asset contract implemented documentation updated

---
+++ src/interfaces/IAsset.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.25;
+
+interface IAsset {
+    error VotesDelegationOnlyOnTokensTransfer();
+    error RoyaltyTransferFailed();
+
+    /// @notice Emitted when the royalty fee has been split successfully between the broker and the pieces market
+    /// @param sender The address that sent the royalty fee
+    /// @param brokerShare The portion of the royalty fee sent to the broker
+    /// @param piecesMarketShare The portion of the royalty fee sent to the pieces market
+    /// @param value The total value of the royalty fee
+    event RoyaltySplitExecuted(address sender, uint256 brokerShare, uint256 piecesMarketShare, uint256 value);
+}
