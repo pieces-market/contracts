061dc7fc68c7cb018ae838a1be0b37a0e80c8f7f
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-01-20 16:56:24 +0100

    emit moved to auctioner

--- src/interfaces/IAsset.sol
+++ src/interfaces/IAsset.sol
@@ -5,11 +5,4 @@ interface IAsset {
     error VotesDelegationOnlyOnTokensTransfer();
     error RoyaltyTransferFailed();
     error InvalidBrokerFee();
-
-    /// @notice Emitted when the royalty fee has been split successfully between the broker and the pieces market
-    /// @param sender The address that sent the royalty fee
-    /// @param brokerShare The portion of the royalty fee sent to the broker
-    /// @param piecesMarketShare The portion of the royalty fee sent to the pieces market
-    /// @param value The total value of the royalty fee
-    event RoyaltySplitExecuted(address sender, uint256 brokerShare, uint256 piecesMarketShare, uint256 value);
 }
