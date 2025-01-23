3b33c9791cbfe9ec28f9118ff2e7cff3f7139656
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-01-23 15:10:35 +0100

    emit royalty event hack script implemented

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.25;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Auctioner} from "../src/Auctioner.sol";
+import {AuctionerDev} from "../src/helpers/AuctionerDev.sol";
 import {Governor} from "../src/Governor.sol";
+import {GovernorDev} from "../src/helpers/GovernorDev.sol";
 
 contract Create is Script {
     function run(address auctioner) external {
@@ -23,6 +25,20 @@ contract Create is Script {
             500,
             5000
         );
+        vm.stopBroadcast();
+    }
+}
+
+/// @dev TMP Script for testing
+contract HackRoyalty is Script {
+    /// @dev EXCLUDE FROM COVERAGE
+    function test() public {}
+
+    function run(address auctioner) external {
+        uint deployerKey = vm.envUint("ADMIN_KEY");
+
+        vm.startBroadcast(deployerKey);
+        AuctionerDev(auctioner).eventHack(11);
         vm.stopBroadcast();
     }
 }
